e6c20ba16f6e4a227eb0327db316240342931c88
Rohan Bhosale <rohanbhosale4229@gmail.com>
2020-10-13 19:15:34 -0400

    brought personal budget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div className="bottom">
+        <footer className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </footer>
+        
+    </div>
+
+  );
+}
+
+export default Footer;
